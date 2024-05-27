6638c6d28a155182460b8610a42dceaac75ff894
Evg Nalivayko <114686372+EvgeniiNalivayko@users.noreply.github.com>
2024-05-27 14:45:24 +0300

    Update antiDoublePhoneMask.js
    PY added

--- antiDoublePhoneMask.js
+++ antiDoublePhoneMask.js
@@ -11,6 +11,8 @@ function getPhonePrefix(countryCode) {
       return "359_12_12";
     case "HR": // Хорватия
       return "385_8_13";
+        case "PY": // Парагвай
+      return "595_8_13";
     case "CO": // Колумбия
       return "57_12_12";
     case "RO": // Румыния
