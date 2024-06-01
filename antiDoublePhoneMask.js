7dfff5c28d68e636d34c5f67573482ffdef33bd4
Evg Nalivayko <114686372+EvgeniiNalivayko@users.noreply.github.com>
2024-06-01 10:46:10 +0300

    Update antiDoublePhoneMask.js
    SV added

--- antiDoublePhoneMask.js
+++ antiDoublePhoneMask.js
@@ -5,9 +5,9 @@ const countryNodeSelector = 'input[name="country_code"]';
 //! Если нет информации по длине телефона пишем просто "ТелефонныйКод", по умолчанию будет мин 6 макс 20.
 function getPhonePrefix(countryCode) {
   switch (countryCode) {
-    case "DZ": // Сальвадор
-      return "+503_8_12"; 
-     case "SV": // Алжир
+    case "SV": // Сальвадор
+      return "503_8_12"; 
+     case "DZ": // Алжир
       return "+213_10_12"; 
     case "MX": // Мексика
       return "52_12_12";
