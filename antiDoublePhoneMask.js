5ec3204588637f31cbeb609de2f511c2da5cd009
Evg Nalivayko <114686372+EvgeniiNalivayko@users.noreply.github.com>
2024-09-05 15:49:25 +0300

    Update antiDoublePhoneMask.js
    GN added

--- antiDoublePhoneMask.js
+++ antiDoublePhoneMask.js
@@ -13,6 +13,8 @@ function getPhonePrefix(countryCode) {
       return "503_8_12";
     case "TZ": // Танзания
       return "255_8_12";
+    case "GN": // Гвинея
+      return "224_8_12"; 
     case "VE": // Венесуэлла
       return "58_8_13";
     case "DZ": // Алжир
